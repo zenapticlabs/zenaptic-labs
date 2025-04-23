720e96f1710a0449d688fd9f600a2baa1bb56735
Rizwan Hanif <rizwanhanif633@gmail.com>
2025-04-23 20:31:05 +0500

    code cleaning

--- src/components/InfiniteSlider.jsx
+++ src/components/InfiniteSlider.jsx
@@ -20,10 +20,7 @@ export default function InfiniteSliderBasic(props) {
            )}
              {component === "Image-Infinite-Slider" && (
                <InfiniteSlider gap={24} direction={animationDirection} speedOnHover={speedOnHover} speed={speed} reverse={direction}>
-                   {featuresList.map((item, index) => (
-                    //    <span key={item} className="px-4 py-3 text-2xl max-md:text-xl rounded-full border border-[#DCDCDC] bg-[rgba(215,215,215,0.2)] whitespace-nowrap max-md:py-2 max-md:px-4">
-                    //        {item}
-                    //    </span>
+                   {featuresList.map((item, index) => (                  
                     <img src={item.src} key={index} alt={item.alt} className="rounded-lg w-[520px]  h-[390px] max-md:w-[45%] object-center object-contain transition-all ease-linear" />
                    ))}
                </InfiniteSlider>
