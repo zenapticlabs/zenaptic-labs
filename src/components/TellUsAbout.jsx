9c58d66110c25402e24473a17aa0b7d6bd854973
Rizwan Hanif <rizwanhanif633@gmail.com>
2025-05-13 22:54:58 +0500

    fixed mobile responsive issues on different devices

--- src/components/TellUsAbout.jsx
+++ src/components/TellUsAbout.jsx
@@ -34,7 +34,7 @@ function TellUsAbout() {
         position: "bottom-right",
         autoClose: 5000,
         hideProgressBar: false,
-        closeOnClick: false,
+        closeOnClick: true,
         pauseOnHover: true,
         draggable: true,
         progress: undefined,
@@ -44,78 +44,63 @@ function TellUsAbout() {
       return;
     }
 
-    try {
-      const res = await fetch("/api/route", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(formData),
-      });
+    const res = await fetch("/api/route", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    });
+    const result = await res.json();
 
-      const result = await res.json();
-
-      if (result.ok) {
-        toast.success(`${result.message}`, {
-          position: "bottom-right",
-          autoClose: 5000,
-          hideProgressBar: false,
-          closeOnClick: false,
-          pauseOnHover: true,
-          draggable: true,
-          progress: undefined,
-          theme: theme,
-          transition: Bounce,
-        });
-
-        setFormData({
-          name: "",
-          email: "",
-          companyName: "",
-          budget: "",
-          projectDescription: "",
-        });
-      } else {
-        toast.error(`${result.message}`, {
-          position: "bottom-right",
-          autoClose: 5000,
-          hideProgressBar: false,
-          closeOnClick: false,
-          pauseOnHover: true,
-          draggable: true,
-          progress: undefined,
-          theme: theme,
-          transition: Bounce,
-        });
-
-        setFormData({
-          name: "",
-          email: "",
-          companyName: "",
-          budget: "",
-          projectDescription: "",
-        });
-      }
-    } catch (error) {
-      toast.error("🔥 Something went wrong, please try again later", {
+    if (result.ok) {
+      toast.success(`${result.message}`, {
         position: "bottom-right",
         autoClose: 5000,
         hideProgressBar: false,
-        closeOnClick: false,
+        closeOnClick: true,
         pauseOnHover: true,
         draggable: true,
         progress: undefined,
         theme: theme,
         transition: Bounce,
       });
+
+      setFormData({
+        name: "",
+        email: "",
+        companyName: "",
+        budget: "",
+        projectDescription: "",
+      });
+    } else {
+      toast.error(`${result.message}`, {
+        position: "bottom-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: theme,
+        transition: Bounce,
+      });
+
+      setFormData({
+        name: "",
+        email: "",
+        companyName: "",
+        budget: "",
+        projectDescription: "",
+      });
     }
   };
 
   return (
     <>
-      <div className="min-w-full px-20 py-18 flex justify-center items-center max-md:bg-[center_top_-135px] bg-[url(/assets/tellusaboutbg-light.png)] tellusbg bg-cover bg-no-repeat bg-[center_top_-30px] flex-col gap-y-10 max-md:px-[20px]">
+      <div className="min-w-full px-20 py-18 flex justify-center items-center max-lg:bg-[center_top_-135px] bg-[url(/assets/tellusaboutbg-light.png)] tellusbg bg-cover bg-no-repeat bg-[center_top_-30px] flex-col gap-y-10 max-lg:px-[20px]">
         <div className="w-full flex justify-center items-center">
-          <h3 className="text-4xl font-bold text-center heading-dark leading-[1.35] from-[#999999] to-[#181818] bg-clip-text text-transparent bg-gradient-to-l max-md:text-[32px]">
+          <h3 className="text-4xl font-bold text-center heading-dark leading-[1.35] from-[#999999] to-[#181818] bg-clip-text text-transparent bg-gradient-to-l max-lg:text-[32px]">
             Tell us about your project
           </h3>
         </div>
@@ -128,7 +113,7 @@ function TellUsAbout() {
                 placeholder="Full Name"
                 value={formData.name}
                 onChange={handleChange}
-                className="w-full border-0 border-b border-[#BDBDBD]  text=[24px] max-md:text-[20px] placeholder-[#BDBDBD] focus:outline-none focus:border-black"
+                className="w-full border-0 border-b border-[#BDBDBD]  text=[24px] max-lg:text-[20px] placeholder-[#BDBDBD] focus:outline-none focus:border-black"
               />
 
               <input
@@ -138,7 +123,7 @@ function TellUsAbout() {
                 placeholder="Email"
                 value={formData.email}
                 onChange={handleChange}
-                className="w-full border-0 border-b border-[#BDBDBD]  text=[24px] max-md:text-[20px] placeholder-[#BDBDBD] focus:outline-none focus:border-black"
+                className="w-full border-0 border-b border-[#BDBDBD]  text=[24px] max-lg:text-[20px] placeholder-[#BDBDBD] focus:outline-none focus:border-black"
               />
 
               <div className="flex flex-col md:flex-row gap-6">
@@ -148,7 +133,7 @@ function TellUsAbout() {
                   placeholder="Company Name"
                   value={formData.companyName}
                   onChange={handleChange}
-                  className="w-full md:flex-1 border-0 border-b border-[#BDBDBD]  text=[24px] max-md:text-[20px] placeholder-[#BDBDBD] focus:outline-none focus:border-black"
+                  className="w-full md:flex-1 border-0 border-b border-[#BDBDBD]  text=[24px] max-lg:text-[20px] placeholder-[#BDBDBD] focus:outline-none focus:border-black"
                 />
                 <input
                   type="text"
@@ -156,7 +141,7 @@ function TellUsAbout() {
                   placeholder="What’s Your Budget? USD"
                   value={formData.budget}
                   onChange={handleChange}
-                  className="w-full md:flex-1 border-0 border-b border-[#BDBDBD]  text=[24px] max-md:text-[20px] placeholder-[#BDBDBD] focus:outline-none focus:border-black"
+                  className="w-full md:flex-1 border-0 border-b border-[#BDBDBD]  text=[24px] max-lg:text-[20px] placeholder-[#BDBDBD] focus:outline-none focus:border-black"
                 />
               </div>
 
@@ -167,13 +152,13 @@ function TellUsAbout() {
                 cols="50"
                 value={formData.projectDescription}
                 onChange={handleChange}
-                className="w-full border-0 border-b border-[#BDBDBD]  text=[24px] max-md:text-[20px] placeholder-[#BDBDBD] focus:outline-none resize-none"
+                className="w-full border-0 border-b border-[#BDBDBD]  text=[24px] max-lg:text-[20px] placeholder-[#BDBDBD] focus:outline-none resize-none"
               ></textarea>
 
               <div className="w-full flex justify-center items-center">
                 <button
                   onClick={handleSubmit}
-                  className="max-md:mb-[78px] bg-[#FF7D12] text-[#F7F7F7] text-[18px] font-bold py-[16px] px-[24px] rounded-[8px]"
+                  className="max-lg:mb-[78px] bg-[#FF7D12] text-[#F7F7F7] text-[18px] font-bold py-[16px] px-[24px] rounded-[8px]"
                 >
                   Send Message
                   <img
@@ -204,9 +189,14 @@ function TellUsAbout() {
       />
 
       <div className="w-full flex justify-center items-center">
-        <InfiniteSlider gap={100} direction={"horizontal"} speed={80} speedOnHover={20}>
+        <InfiniteSlider
+          gap={100}
+          direction={"horizontal"}
+          speed={80}
+          speedOnHover={20}
+        >
           <span
-            className=" text-transparent max-md:!text-[36px] stroke-text  text-center font-urbanist font-bold text-[56px] leading-[135%]"
+            className=" text-transparent max-lg:!text-[36px] stroke-text  text-center font-urbanist font-bold text-[56px] leading-[135%]"
             style={{
               WebkitTextStroke: "1px #686868",
             }}
@@ -214,7 +204,7 @@ function TellUsAbout() {
             TRANSFORMING SOFTWARE IDEAS FOR YOUR BUSINESS INTO LIFE
           </span>
           <span className="w-[24px] h-[24px] z-1 rounded bg-gradient-to-l self-center from-orange-500 to-amber-300 shadow-[0px_0px_32px_8px_rgba(255,179,120,0.25)] lamp"></span>
-          <span className="  from-[#999999] max-md:!text-[36px] to-[#181818] bg-clip-text text-transparent bg-gradient-to-r text-[56px] font-extrabold leading-[135%]">
+          <span className="  from-[#999999] max-lg:!text-[36px] to-[#181818] bg-clip-text text-transparent bg-gradient-to-r text-[56px] font-extrabold leading-[135%]">
             TRANSFORMING SOFTWARE IDEAS FOR YOUR BUSINESS INTO LIFE
           </span>
           <span className="w-[24px] h-[24px] z-1 rounded bg-gradient-to-l self-center from-orange-500 to-amber-300 shadow-[0px_0px_32px_8px_rgba(255,179,120,0.25)] lamp"></span>
