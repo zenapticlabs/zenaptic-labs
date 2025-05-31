2e73fce942d83b201f70e278d98d7daf41557df7
sunghyunkang1111 <114709653+sunghyunkang1111@users.noreply.github.com>
2025-05-31 17:40:54 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,12 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
-import node from '@astrojs/node'; 
+// import node from '@astrojs/node'; 
 export default defineConfig({
-  output: 'server',
-  adapter: node({
-    mode: 'standalone' 
-  }),
+  // output: 'server',
+  // adapter: node({
+  //   mode: 'standalone' 
+  // }),
   vite: {
     plugins: [tailwindcss()],
   },
