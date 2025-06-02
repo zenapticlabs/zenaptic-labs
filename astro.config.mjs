3b8226007d0f6c06d925ada489332b71de51c1b0
Sung-Hyun Kang <sunghyunkang@microsoft.com>
2025-06-01 23:43:10 -0500

    Added vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,14 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
 // import node from '@astrojs/node'; 
+import vercel from '@astrojs/vercel/serverless';
+
 export default defineConfig({
   // output: 'server',
   // adapter: node({
   //   mode: 'standalone' 
   // }),
+  adapter: vercel(),
   vite: {
     plugins: [tailwindcss()],
   },
